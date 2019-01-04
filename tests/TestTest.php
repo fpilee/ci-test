a9f6f9d62f7abf4b5419a04b3881a4a7f41f9a7d
Felipe Morales <fpilee@gmail.com>
2019-01-04 18:26:50 -0300

    added phunit test and config

---
+++ tests/TestTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Felipe Morales <fpilee@gmail.com>.
+ * Date: 04/01/19
+ * Time: 18:10
+ */
+
+
+class TestTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testTestOne()
+    {
+        $this->assertEquals(1,1);
+    }
+}
